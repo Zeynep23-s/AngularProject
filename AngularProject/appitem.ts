e2f2161f26582176a8364b256449c12110907f35
Zeynep23-s <85453575+Zeynep23-s@users.noreply.github.com>
2021-09-03 01:41:16 +0300

    Create appitem.ts

---
+++ AngularProject/appitem.ts
@@ -0,0 +1,43 @@
+//export class appItem {
+    //EvcilHayvanlar:string;
+    //Sahiplenici:string;
+
+   // constructor(EvcilHayvanlar:string,Sahiplenici:string) {
+      //  this.EvcilHayvanlar=EvcilHayvanlar;
+       // this.Sahiplenici=Sahiplenici;
+    
+
+export interface appItem {
+    Uyeler:  string;
+    
+    
+
+}
+
+export const products=[
+    {
+    id:1,
+    name:"Köpek",
+    quantity:0,
+    photoPath:"assets/images/köp.jpg"
+
+    },
+    {
+        id:2,
+        name:"Kedi",
+        quantity:0,
+        photoPath:"assets/images/kedi.jpg"
+    },
+    {
+        id:3,
+        name:"Kaplumbağa",
+        quantity:0,
+        photoaPath:"assets/images/kap.jpg"
+    },
+   {
+    id:4,
+    name:"Kuş",
+    quantity:0,
+    photoPath:"assets/images/kuş.jpg"
+   },
+]
